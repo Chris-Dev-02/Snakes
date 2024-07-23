afba3df5ecd386c5df7abc4a80c733c9dcfe805b
chris <ChristopherDeveloper3@gmail.com>
2024-07-23 04:12:32 -0600

    All the missing functionality of a basic snake was added, since the snake's tail is drawn properly, the apple is also drawn in a random position, and finally the snake already has a collision with the frame, with the apple and with itself.

--- My Snake/main.c
+++ My Snake/main.c
@@ -12,7 +12,8 @@ struct position upper_limit = {0, 0};
 struct position lower_limit = {80, 20};
 struct position apple = {10, 10};
 enum direction direction = LEFT; 
-int prev_snake_lenght = 1;
+int sleep_time = 100;
+float seconds = 0.0;
 int snake_lenght = 1;
 bool game_over = false;
 int score = 0;
@@ -23,6 +24,7 @@ void gotoxy(struct position pos)
 
 }
 
+// Drawings functions
 void draw_frame(){
     for(int i = upper_limit.x; i < lower_limit.x; i++){
         struct position pos_1 = {i, upper_limit.y};
@@ -57,19 +59,54 @@ void draw_snake(){
     for(int i = 0; i < snake_lenght; i++){
         gotoxy(snake[i]);
         if(i == 0){
-            printf("@");
+            game_over ? printf("X") : printf("@");
         }
         else{
-            printf("*");
+            game_over ? printf("+") : printf("*");
         }
     }
 }
 
-void erase_snake(){
-    for(int i = 0; i < snake_lenght; i++){
-        gotoxy(snake[0]);
-        printf(" ");
+void draw_score(){
+    struct position score_pos = {0, lower_limit.y + 1};
+    gotoxy(score_pos);
+    printf("SCORE: ");
+    struct position time_position = {0, lower_limit.y + 2};
+    gotoxy(time_position);
+    printf("TIME: ");
+}
+
+void update_and_redraw_score(){
+    struct position score_pos = {8, lower_limit.y + 1};
+    gotoxy(score_pos);
+    printf("%i", score);
+    struct position time_position = {8, lower_limit.y + 2};
+    gotoxy(time_position);
+    printf("%.f", seconds);
+}
+
+// General
+void draw_new_apple(){
+    apple.x = rand() % (lower_limit.x - upper_limit.x + 1) + upper_limit.x;
+    apple.y = rand() % (lower_limit.y - upper_limit.y + 1) + upper_limit.y;
+
+    if(apple.x == 0 || apple.y == 0 || apple.x == lower_limit.x || apple.y == lower_limit.y){
+        return draw_new_apple();
     }
+
+    gotoxy(apple);
+    printf("%c", 224);
+}
+
+void erase_snake(){
+    /*
+        for(int i = 0; i < snake_lenght; i++){
+        gotoxy(snake[i]);
+        printf(" ");
+    }*/
+    
+    gotoxy(snake[snake_lenght - 1]);
+    printf(" ");
 }
 
 void read_keyboard(){
@@ -84,40 +121,79 @@ void read_keyboard(){
 }
 
 void move_snake(){
-    switch (direction)
-    {
-    case RIGHT:
-        snake[0].x++;
-        break;
-    case UP: 
-        snake[0].y--;
-        break;
-    case LEFT:
-        snake[0].x--;
-        break;
-    case DOWN:
-        snake[0].y++;
-        break;
-    default:
-        break;
-    }
-
-    for(int i = 0; i < snake_lenght; i++){
-
+    if(!game_over){
+        struct position aux_1, aux_2;
+        for(int i = snake_lenght - 1; i > 0; i--){
+            snake[i] = snake[i - 1];
+        }
+            
+        switch (direction)
+        {
+        case RIGHT:
+            snake[0].x++;
+            break;
+        case UP: 
+            snake[0].y--;
+            break;
+        case LEFT:
+            snake[0].x--;
+            break;
+        case DOWN:
+            snake[0].y++;
+            break;
+        default:
+            break;
+        }
     }
 }
 
+void calculate_time(){
+    seconds = seconds + (sleep_time / 1000.0);
+}
+
+// Collision functions
+void check_collision_with_apple(){
+    if(snake[0].x == apple.x && snake[0].y == apple.y){
+        snake[snake_lenght + 1] = snake[snake_lenght];
+        draw_new_apple();
+        score += 1;
+        snake_lenght++;
+    }
+}
+
+void check_collision_with_frame(){
+    if(snake[0].x == upper_limit.x || snake[0].x == lower_limit.x){
+        game_over = true;
+    }
+    if(snake[0].y == upper_limit.y || snake[0].y == lower_limit.y){
+        game_over = true;
+    }
+}
+
+void check_collision_with_snake(){
+    for (int i = 1; i < snake_lenght; i++) {
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y)
+            game_over = true;
+    }
+}
 
 int main()
 {
     snake[0] = snake_init_pos;
     draw_frame();
-    while(1){
+    draw_score();
+    draw_new_apple();
+    while(!game_over){
         erase_snake();
         read_keyboard();
+        check_collision_with_frame();
+        check_collision_with_snake();
+        check_collision_with_apple();
         move_snake();
         draw_snake();
-        Sleep(100);
+        update_and_redraw_score();
+        Sleep(sleep_time);
+        calculate_time();
     }
 
     return 0;
